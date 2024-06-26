fe62ee1e5814c944ad5f0627a9d54f2e55fd9f99
James Roseman <james.roseman@gmail.com>
2024-06-26 10:20:05 +0000

    Simplify setup

--- test-beam/setup.py
+++ test-beam/setup.py
@@ -1,13 +1,5 @@
-import os
-import subprocess
-from setuptools import setup, find_packages, Command
+from setuptools import setup, find_packages
 
-import boto3
-
-
-# Your S3 bucket details
-S3_BUCKET = 'beam'
-S3_TEMP_LOCATION = 's3://tmp/'
 
 # Define your package details
 PACKAGE_NAME = 'test_beam_pipelines'
@@ -18,33 +10,6 @@ REQUIREMENTS = [
     'boto3',  # Add boto3 to your requirements
 ]
 
-
-class UploadToS3Command(Command):
-    description = 'Build and upload the package to S3'
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        # Build the distribution
-        self.run_command('sdist')
-
-        # Upload the distribution to S3
-        dist_file = f'dist/{PACKAGE_NAME}-{PACKAGE_VERSION}.tar.gz'
-        s3_key = os.path.join('temp', f'{PACKAGE_NAME}-{PACKAGE_VERSION}.tar.gz')
-        s3 = boto3.client('s3')
-
-        if os.path.exists(dist_file):
-            s3.upload_file(dist_file, S3_BUCKET, s3_key)
-            print(f'Uploaded {dist_file} to s3://{S3_BUCKET}/{s3_key}')
-        else:
-            print(f"Error: {dist_file} does not exist. Make sure the package has been built before uploading.")
-
-
 # Standard setup
 setup(
     name=PACKAGE_NAME,
@@ -52,7 +17,4 @@ setup(
     description=DESCRIPTION,
     packages=find_packages(),
     install_requires=REQUIREMENTS,
-    cmdclass={
-        'upload_to_s3': UploadToS3Command,
-    },
 )
